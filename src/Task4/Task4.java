686f557a26c1d7ab770f294724ae625413618735
JennySmirnova <sunkowshik@rambler.ru>
2019-09-03 10:52:02 +0300

    Добавление задачи 4

--- src/Task4/Task4.java
+++ src/Task4/Task4.java
@@ -1,4 +1,11 @@
 package Task4;
-
+import java.util.Scanner;
 public class Task4 {
+     public static void main(String args[]){
+         Scanner in = new Scanner(System.in);
+         System.out.print("Введите число в бинарном формате: ");
+         String num = in.next();
+         System.out.println("Число в десятичном формате: "+ Integer.parseInt(num,2));
+     }
 }
+
