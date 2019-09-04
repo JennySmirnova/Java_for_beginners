27f38a290585ac3d5553c851c6855b3043dbb12d
JennySmirnova <sunkowshik@rambler.ru>
2019-09-04 09:54:47 +0300

    Выполнение восьмого задания: цикличный вывод данных на экран

---
+++ src/Task8/Task8.java
@@ -0,0 +1,12 @@
+package Task8;
+
+public class Task8 {
+   public static void main(String args[]) {
+       int n = 1;
+       while (n < 51) {
+
+           System.out.println(n);
+           n++;
+       }
+   }
+}
