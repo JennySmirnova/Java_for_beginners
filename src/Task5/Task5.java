cfb535c8038850b6df66a9d7fbeb0840d51b34e3
JennySmirnova <sunkowshik@rambler.ru>
2019-09-04 08:07:42 +0300

    Выполнение пятого задания

---
+++ src/Task5/Task5.java
@@ -0,0 +1,32 @@
+package Task5;
+import java.util.Scanner;
+public class Task5 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int a = in.nextInt();
+        int b=a*1;
+        int c=a*2;
+        int d=a*3;
+        int e=a*4;
+        int f=a*5;
+        int g=a*6;
+        int h=a*7;
+        int j=a*8;
+        int k=a*9;
+        int l=a*10;
+
+        System.out.println("1*" + a + "=" + b);
+        System.out.println("2*" + a + "=" + c);
+        System.out.println("3*" + a + "=" + d);
+        System.out.println("4*" + a + "=" + e);
+        System.out.println("5*" + a + "=" + f);
+        System.out.println("6*" + a + "=" + g);
+        System.out.println("7*" + a + "=" + h);
+        System.out.println("8*" + a + "=" + j);
+        System.out.println("9*" + a + "=" + k);
+        System.out.println("10*" + a + "=" + l);
+    }
+}
+
+
